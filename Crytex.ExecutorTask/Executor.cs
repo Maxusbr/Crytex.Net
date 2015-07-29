b37784df4b6d3f6836c2d501009d33672391e45d
denakol <denakol08@gmail.com>
2015-07-29 22:18:12 +0300

    executor

--- Crytex.ExecutorTask/Executor.cs
+++ Crytex.ExecutorTask/Executor.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Crytex.ExecutorTask.Hyper_V;
+using Crytex.ExecutorTask.VmWare;
 
 namespace Crytex.ExecutorTask
 {
     public class Executor
     {
+        private HyperVExecutor _hyperVExecutor = new HyperVExecutor();
+        private VmWareExecutor _vmWareExecutor = new VmWareExecutor();
+
+
+
     }
 }
---
+++ Crytex.WCF.Receiver/ControlService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Crytex.WCF.Receiver
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ControlService" в коде и файле конфигурации.
+    public class ControlService : IControlService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
