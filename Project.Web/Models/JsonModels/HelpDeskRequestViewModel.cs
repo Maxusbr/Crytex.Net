01dc68d59d8efe4693e1e7ec031ca497699c3135
Andrey Sasimovich <ballack.tyta@gmail.com>
2015-08-05 21:42:58 +0300

    Fix: Add forgotten model file

---
+++ Project.Web/Models/JsonModels/HelpDeskRequestViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Project.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project.Web.Models.JsonModels
+{
+    public class HelpDeskRequestViewModel
+    {
+        [Required]
+        public string Summary { get; set; }
+
+        [Required]
+        public string Details { get; set; }
+
+        public RequestStatus Status { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int Id { get; set; }
+    }
+}
