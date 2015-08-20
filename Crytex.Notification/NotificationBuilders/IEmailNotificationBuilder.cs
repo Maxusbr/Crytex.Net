f08424cad6ec9fa59a70e2bb3070b58e9f7dfc27
Andrey Sasimovich <ballack.tyta@gmail.com>
2015-08-20 16:31:01 +0300

    Add NotificationManager class. Add sender and builder interfaces. Update ignore file

---
+++ Crytex.Notification/NotificationBuilders/IEmailNotificationBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Project.Model.Models.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Crytex.Notification.NotificationBuilders
+{
+    public interface IEmailNotificationBuilder
+    {
+        // TODO: change return type to Email
+        object Build(ExampleNotification notification);
+    }
+}
