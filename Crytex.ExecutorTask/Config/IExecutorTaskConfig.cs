40195c062ac10faca4f82636f074668c2deb591f
Andrey Sasimovich <ballack.tyta@gmail.com>
2015-08-28 16:04:49 +0300

    Add base IAppConfig and AppConfig, move the logic from ServerConfig to this class. Add to each high-level project its own Config derived from base config interface and class

--- Crytex.ExecutorTask/Config/IExecutorTaskConfig.cs
+++ Crytex.ExecutorTask/Config/IExecutorTaskConfig.cs
@@ -3,11 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Project.Web.Service
+namespace Crytex.ExecutorTask.Config
 {
-    public interface IServerConfig : IAppConfig
+    public interface IExecutorTaskConfig : IAppConfig
     {
-        
     }
 }
