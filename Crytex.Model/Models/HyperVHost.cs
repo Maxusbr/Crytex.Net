e3dbb5e7e4f519908c5a00adff9a17ea2dfeea44
Andrey Sasimovich <ballack.tyta@gmail.com>
2015-09-10 13:00:23 +0300

    Update domain model. Add corresponding migration

---
+++ Crytex.Model/Models/HyperVHost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytex.Model.Models
+{
+    public class HyperVHost
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Host { get; set; }
+        public int CoreNumber { get; set; }
+        public int RamSize { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool Valid { get; set; }
+        public DateTime DateAdded { get; set; }
+        public Guid SystemCenterVirtualManagerId { get; set; }
+
+        [ForeignKey("SystemCenterVirtualManagerId")]
+        public SystemCenterVirtualManager VirtualManager { get; set; }
+        [InverseProperty("HyperVHost")]
+        public virtual ICollection<HyperVHostResource> Resources { get; set; }
+    }
+}
---
+++ Crytex.Model/Models/SystemCenterVirtualManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytex.Model.Models
+{
+    public class SystemCenterVirtualManager
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Host { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool Synchronize { get; set; }
+        public string Name { get; set; }
+
+        [InverseProperty("VirtualManager")]
+        public virtual ICollection<HyperVHost> HyperVHosts { get; set; }
+    }
+}
