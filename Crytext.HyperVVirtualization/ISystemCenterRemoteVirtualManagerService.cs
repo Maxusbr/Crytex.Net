17d3f977a3b1ba9a35261232ce50c27641bea928
Andrey Sasimovich <ballack.tyta@gmail.com>
2015-09-10 15:03:10 +0300

    Add HyperVVirtualization project. Add ISystemCenterVirtualManagerService interface

---
+++ Crytext.HyperVVirtualization/ISystemCenterRemoteVirtualManagerService.cs
@@ -0,0 +1,15 @@
+﻿using Crytex.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytext.HyperVVirtualization
+{
+    public interface ISystemCenterRemoteVirtualManagerService
+    {
+        bool Check(SystemCenterVirtualManager manager);
+        IEnumerable<HyperVHost> GetHyperVMHosts(SystemCenterVirtualManager manager);
+    }
+}
