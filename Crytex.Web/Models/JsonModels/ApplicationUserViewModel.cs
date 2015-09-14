14069f8b7eaf2bda6cfdbbee6db6d388ba394295
Andrey Sasimovich <ballack.tyta@gmail.com>
2015-09-14 19:50:40 +0300

    Update UserController with Post, Put and Delete methods

--- Crytex.Web/Models/JsonModels/ApplicationUserViewModel.cs
+++ Crytex.Web/Models/JsonModels/ApplicationUserViewModel.cs
@@ -1,10 +1,31 @@
-﻿namespace Crytex.Web.Models.JsonModels
+﻿using System.ComponentModel.DataAnnotations;
+namespace Crytex.Web.Models.JsonModels
 {
     public class ApplicationUserViewModel
     {
         public string Id { get; set; }
         public string UserName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        public string Password { get; set; }
 
+        public bool ValidateForCreationScenario()
+        {
+            if (string.IsNullOrEmpty(this.UserName)) return false;
+            if (string.IsNullOrEmpty(this.Email)) return false;
+            if (string.IsNullOrEmpty(this.Password)) return false;
+
+            return true;
+        }
+
+        public bool ValidateForEditingScenario()
+        {
+            if (string.IsNullOrEmpty(this.UserName) && string.IsNullOrEmpty(this.Email) && string.IsNullOrEmpty(this.Password))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
