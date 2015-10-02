13542cb90b0f4b3fc8feae80d1567ef695333689
Andrey Sasimovich <ballack.tyta@gmail.com>
2015-10-02 19:01:42 +0300

    Add CreateVm method to VmWareControl

--- Crytex.ExecutorTask/TaskHandler/VmWare/IVmWareControl.cs
+++ Crytex.ExecutorTask/TaskHandler/VmWare/IVmWareControl.cs
@@ -1,6 +1,14 @@
-﻿namespace Crytex.ExecutorTask.TaskHandler.VmWare
+﻿using Crytex.Model.Models;
+using System;
+namespace Crytex.ExecutorTask.TaskHandler.VmWare
 {
     public interface IVmWareControl
     {
+        /// <summary>
+        /// Создание виртуальной машины
+        /// </summary>
+        /// <param name="task">Объект задачи создания</param>
+        /// <returns></returns>
+        Guid CreateVm(CreateVmTask task);
     }
 }
---
+++ Crytex.ExecutorTask/TaskHandler/VmWare/VmWareControl.cs
@@ -0,0 +1,43 @@
+﻿using Crytex.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VmWareRemote.Implementations;
+using VmWareRemote.Interface;
+
+namespace Crytex.ExecutorTask.TaskHandler.VmWare
+{
+    public class VmWareControl : IVmWareControl
+    {
+        private IVmWareProvider _vmWareProvider;
+
+        public VmWareControl(IVmWareProvider vmWareProvider)
+        {
+            this._vmWareProvider = vmWareProvider;
+        }
+
+        public Guid CreateVm(CreateVmTask task)
+        {
+            // Connect to server
+            ConnectIfNotConnected();
+            
+            // Generate unique server machine-name by generating new Guid
+            var machineGuid = Guid.NewGuid();
+
+            // Create new vm
+            this._vmWareProvider.CreateVm(machineGuid.ToString(), task.Cpu, task.Ram);
+
+            return machineGuid;
+        }
+
+        private void ConnectIfNotConnected()
+        {
+            if (this._vmWareProvider.ConnectionState != ConnectionState.Connected)
+            {
+                this._vmWareProvider.Connect();
+            }
+        }
+    }
+}
