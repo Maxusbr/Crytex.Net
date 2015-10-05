18d0f960ae8d672f4c5da8e19a1fe2539d7da86f
Andrey Sasimovich <ballack.tyta@gmail.com>
2015-10-05 13:18:53 +0300

    Add methods to VmWareControl

--- Crytex.ExecutorTask/TaskHandler/VmWare/VmWareControl.cs
+++ Crytex.ExecutorTask/TaskHandler/VmWare/VmWareControl.cs
@@ -1,9 +1,11 @@
-﻿using Crytex.Model.Models;
+﻿using Crytex.Model.Exceptions;
+using Crytex.Model.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VmWareRemote.Exceptions;
 using VmWareRemote.Implementations;
 using VmWareRemote.Interface;
 
@@ -27,11 +29,80 @@ namespace Crytex.ExecutorTask.TaskHandler.VmWare
             var machineGuid = Guid.NewGuid();
 
             // Create new vm
-            this._vmWareProvider.CreateVm(machineGuid.ToString(), task.Cpu, task.Ram);
+            var ramMB = task.Ram * 1024;
+            this._vmWareProvider.CreateVm(machineGuid.ToString(), task.Cpu, ramMB);
 
             return machineGuid;
         }
 
+
+        public void UpdateVm(UpdateVmTask updateVmTask)
+        {
+            // Connect to server
+            ConnectIfNotConnected();
+
+            try
+            {
+                // Get server machine-name from machine Id
+                var machineName = updateVmTask.VmId.ToString();
+
+                // Calculate Ram size in megabytes
+                var ramMB = updateVmTask.Ram * 1024;
+
+                // Update vm conf by calling provider's ModifyMachine
+                this._vmWareProvider.ModifyMachine(machineName, updateVmTask.Cpu, ramMB);
+            }
+            // Handle invalid name exceptions
+            catch (InvalidNameException e)
+            {
+                throw new InvalidIdentifierException("Invalid name. See inner exception", e);
+            }
+        }
+
+        public void StartVm(string machineName)
+        {
+            this.StandartOperationInner(machineName, TypeStandartVmTask.Start);
+        }
+
+        public void StopVm(string machineName)
+        {
+            this.StandartOperationInner(machineName, TypeStandartVmTask.Stop);
+        }
+
+        public void RemoveVm(string machineName)
+        {
+            this.StandartOperationInner(machineName, TypeStandartVmTask.Remove);
+        }
+
+        #region Private methods
+
+        private void StandartOperationInner(string machineName, TypeStandartVmTask typeStandartVmTask)
+        {
+            // Connect to server
+            ConnectIfNotConnected();
+
+            try
+            {
+                switch (typeStandartVmTask)
+                {
+                    case TypeStandartVmTask.Start:
+                        this._vmWareProvider.StartMachine(machineName);
+                        break;
+                    case TypeStandartVmTask.Stop:
+                        this._vmWareProvider.StopMachine(machineName);
+                        break;
+                    case TypeStandartVmTask.Remove:
+                        this._vmWareProvider.DeleteMachine(machineName);
+                        break;
+                }
+            }
+            // Handle invalid name exceptions
+            catch (InvalidNameException e)
+            {
+                throw new InvalidIdentifierException("Invalid name. See inner exception", e);
+            }
+        }
+        
         private void ConnectIfNotConnected()
         {
             if (this._vmWareProvider.ConnectionState != ConnectionState.Connected)
@@ -39,5 +110,6 @@ namespace Crytex.ExecutorTask.TaskHandler.VmWare
                 this._vmWareProvider.Connect();
             }
         }
+        #endregion // Private methods
     }
 }
