6b11ce5b37424f72c2e974e0bcb59215f0d41954
morow93 <derchepur@gmail.com>
2015-10-13 23:35:07 +0300

    Написал OAuth провайдеров 1)Работает получение access_token 2)Работает получение refresh_token 3)Есть возможность указания client_id 4)Добавил CORS

---
+++ Crytex.Data/Repository/OAuthRefreshTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using Crytex.Data.Infrastructure;
+using Crytex.Data.IRepository;
+using Crytex.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytex.Data.Repository
+{
+    public class OAuthRefreshTokenRepository : RepositoryBase<OAuthRefreshToken>, IOAuthRefreshTokenRepository
+    {
+        public OAuthRefreshTokenRepository(IDatabaseFactory databaseFactory)
+                : base(databaseFactory)
+        { }
+    }
+}
---
+++ Crytex.Service/IService/IOAuthService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crytex.Model.Models;
+using Crytex.Service.Model;
+
+namespace Crytex.Service.IService
+{
+    public interface IOAuthService
+    {
+        OAuthClientApplication FindClient(String clientId);
+        OAuthRefreshToken FindRefreshToken(String refreshTokenId);
+        Boolean AddRefreshToken(OAuthRefreshToken token);
+        Boolean RemoveRefreshToken(String refreshTokenId);
+        Boolean RemoveRefreshToken(OAuthRefreshToken refreshToken);
+        Boolean RemoveRefreshToken(RemoveRefreshTokenParams model);
+        Boolean RemoveAllExpiredRefreshTokens();
+    }
+}
