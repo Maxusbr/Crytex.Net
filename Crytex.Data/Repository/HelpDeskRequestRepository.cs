415538c21d492a5d03a37fdb27a0015730441ab9
Ruslan <Ruslan.Zh28@gmail.com>
2015-10-18 21:44:17 +0300

    Crytex-164 HelpDeskRequest Filter with Read Unread All

--- Crytex.Data/Repository/HelpDeskRequestRepository.cs
+++ Crytex.Data/Repository/HelpDeskRequestRepository.cs
@@ -1,6 +1,11 @@
-﻿using Crytex.Data.Infrastructure;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using Crytex.Data.Infrastructure;
 using Crytex.Data.IRepository;
 using Crytex.Model.Models;
+using PagedList;
 
 namespace Crytex.Data.Repository
 {
@@ -9,6 +14,29 @@ namespace Crytex.Data.Repository
         public HelpDeskRequestRepository(IDatabaseFactory databaseFactory)
             : base(databaseFactory)
         {
+        }
+
+        public  IPagedList<HelpDeskRequest> GetPage<TOrder, TSecondOrder>(Page page, Expression<Func<HelpDeskRequest, bool>> where, Expression<Func<HelpDeskRequest, TOrder>> order, Expression<Func<HelpDeskRequest, TSecondOrder>> secondOrder)
+        {
+            var query = this.DataContext.HelpDeskRequests
+                .Where(where)
+                .OrderBy(order);
+            if (secondOrder != null) query = query.ThenByDescending(secondOrder);
+            var pageQuery = query.GetPage(page);
+
+            var finalQuery = this.AppendIncludesToRequest(pageQuery);
+
+            var results = finalQuery.ToList();
+            var total = results.Count();
+
+            return new StaticPagedList<HelpDeskRequest>(results, page.PageNumber, page.PageSize, total);
+        }
+
+        private IQueryable<HelpDeskRequest> AppendIncludesToRequest(IQueryable<HelpDeskRequest> query)
+        {
+            query = query
+                .Include(vm => vm.User);
+            return query;
         }
     }
 }
---
+++ Crytex.Service/Model/HelpDeskRequestFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytex.Service.Model
+{
+    public enum HelpDeskRequestFilter
+    {
+        Unread = 0,
+        Read,
+        All
+    }
+}
