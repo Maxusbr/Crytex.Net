068aee3b017bd97092b5b30ac2b78e45856eb4dd
Ruslan <Ruslan.Zh28@gmail.com>
2015-11-04 20:15:33 +0300

    Crytex-179 StatisticJob.cs DateTimeExtension.cs

---
+++ Crytex.Background/Tasks/StatisticJob.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Crytex.Model.Models;
+using Crytex.Service.IService;
+using Quartz;
+
+namespace Crytex.Background.Statistic
+{
+    public class StatisticJob: IJob
+    {
+        private IStatisticService _statisticService { get; set; }
+        private TypeStatistic typeStatistic { get; set; }
+
+        public StatisticJob(IStatisticService statisticService)
+        {
+            this._statisticService = statisticService;
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            JobDataMap dataMap = context.MergedJobDataMap;
+            typeStatistic = (TypeStatistic)dataMap["typeStatistic"];
+
+            this._statisticService.CalculateStatistic(typeStatistic);
+            Console.WriteLine("It's billing " + context.JobDetail.Key.Name);
+        }
+    }
+}
---
+++ Crytex.Core/Extension/DateTimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytex.Core.Extension
+{
+    public static class DateTimeExtension
+    {
+        public static DateTime TrimDate(this DateTime date, long roundTicks)
+        {
+            return new DateTime(date.Ticks - date.Ticks % roundTicks);
+        }
+    }
+}
