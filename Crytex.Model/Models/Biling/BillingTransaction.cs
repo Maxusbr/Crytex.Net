2f778e89d1752de863bee06e96213d6cee53d228
derchepur <derchepur@gmail.com>
2015-11-24 14:00:11 +0300

    дописал модели билинга

--- Crytex.Model/Models/Biling/BillingTransaction.cs
+++ Crytex.Model/Models/Biling/BillingTransaction.cs
@@ -5,13 +5,23 @@ namespace Crytex.Model.Models
 {
     public class BillingTransaction : BaseEntity
     {
-        public string UserId { get; set; }
+      
         public BillingTransactionType TransactionType { get; set; }
         public DateTime Date { get; set; }
         public decimal CashAmount { get; set; }
+       
+        public string Currency { get; set; }
+        public string Description { get; set; }
 
+
+        public string UserId { get; set; }
         [ForeignKey("UserId")]
         public ApplicationUser User { get; set; }
+
+
+        public string UserVmId { get; set; }
+        [ForeignKey("UserVm")]
+        public UserVm UserVm { get; set; }
     }
 
     public enum BillingTransactionType
---
+++ Crytex.Model/Models/Biling/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crytex.Model.Models
+{
+    public class Payment : GuidBaseEntity
+    {
+        public decimal Amount { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime? DateEnd { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public bool Succes { get; set; }
+        public TypePayment TypePayment { get; set; }
+    }
+
+    public enum TypePayment
+    {
+        Interkassa = 0,
+        Onpay = 1
+    }
+
+}
