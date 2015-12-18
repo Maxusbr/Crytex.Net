ed5426c3dd134367c409183729f706b3d57285d5
AndreySasimovich <ballack.tyta@gmail.com>
2015-12-18 14:18:46 +0300

    Add "not enough money" exception handling

---
+++ Crytex.Model/Exceptions/TransactionFailedException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Crytex.Model.Exceptions
+{
+    public class TransactionFailedException : ApplicationException
+    {
+        public TransactionFailedException(string message) : base(message) { }
+    }
+}
