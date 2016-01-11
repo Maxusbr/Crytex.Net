87b370cc164570e5099addea5a42a9b93101c286
denakol <denakol08@gmail.com>
2016-01-11 13:10:23 +0300

    add virtualization and fakeprovider

---
+++ Crytex.Virtulization.Fake/FakeProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crytex.Virtualization.Base;
+using Crytex.Virtualization.Base.VMModify;
+
+namespace Crytex.Virtulization.Fake
+{
+    public class FakeProvider: IProviderVM
+    {
+        public ProviderVirtualization CurrentProvider { get; }
+        public ReturnedRezultes ConnectToServer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IVMachine> GetAllMachines()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVMachine GetMachinesByName(string machineName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReturnedRezultes GetNetworkSwithes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReturnedRezultes CreateMachine(VMModifySpecification spec)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
