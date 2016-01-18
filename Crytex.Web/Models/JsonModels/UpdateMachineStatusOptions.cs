c168eea2253eb62a5d86b1d518c6a0add6f12207
AndreySasimovich <ballack.tyta@gmail.com>
2016-01-18 19:58:44 +0300

    Missed model file fix

---
+++ Crytex.Web/Models/JsonModels/UpdateMachineStatusOptions.cs
@@ -0,0 +1,10 @@
+﻿using Crytex.Model.Models;
+
+namespace Crytex.Web.Models.JsonModels
+{
+    public class UpdateMachineStatusOptions
+    {
+        public TypeChangeStatus Status { get; internal set; }
+        public string SubscriptionId { get; internal set; }
+    }
+}
