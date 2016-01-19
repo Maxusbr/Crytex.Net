e66c3f75c4ce24528fc1a3ec81f5599fc4ae3bbe
AndreySasimovich <ballack.tyta@gmail.com>
2016-01-19 19:12:50 +0300

    Add Create and Update vm task handlers

---
+++ Crytex.ExecutorTask/TaskHandler/NewTaskHandler/UpdateVmTaskHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Crytex.Model.Models;
+using Crytex.Virtualization.Base;
+using System.Linq;
+
+namespace Crytex.ExecutorTask.TaskHandler
+{
+    internal class UpdateVmTaskHandler : BaseNewTaskHandler
+    {
+        public UpdateVmTaskHandler(TaskV2 task, IProviderVM virtualizationProvider, Guid virtualizationServerEntityId) 
+            : base(task, virtualizationProvider, virtualizationServerEntityId)
+        {
+        }
+
+        protected override TaskExecutionResult ExecuteLogic()
+        {
+            Console.WriteLine("Update vm task VmWare");
+            var taskExecutionResult = new CreateVmTaskExecutionResult();
+            try
+            {
+                var updateVmTaskOptions = this.TaskEntity.GetOptions<UpdateVmOptions>();
+
+                this.VirtualizationProvider.ConnectToServer();
+
+                var vm = this.VirtualizationProvider.GetMachinesByName(updateVmTaskOptions.VmId.ToString());
+
+                vm.NumCPU = updateVmTaskOptions.Cpu;
+                vm.Memory = updateVmTaskOptions.Ram;
+                vm.VirtualDrives.Drives.First().ResizeDisk(updateVmTaskOptions.Hdd);
+                vm.Modify();
+
+                taskExecutionResult.Success = true;
+            }
+            catch (Exception ex)
+            {
+                taskExecutionResult.Success = false;
+                taskExecutionResult.ErrorMessage = ex.Message;
+            }
+
+            return taskExecutionResult;
+        }
+    }
+}
