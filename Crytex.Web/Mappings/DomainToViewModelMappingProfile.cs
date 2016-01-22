8d3ac450647dca98e9515585c02d5a7c94c8aaa9
denakol <denakol08@gmail.com>
2016-01-22 14:00:40 +0300

    fix

--- Crytex.Web/Mappings/DomainToViewModelMappingProfile.cs
+++ Crytex.Web/Mappings/DomainToViewModelMappingProfile.cs
@@ -104,7 +104,8 @@ namespace Crytex.Web.Mappings
                 .ForMember(x => x.UserName, opt => opt.MapFrom(source => source.User.UserName));
             Mapper.CreateMap<PaymentGameServer, PaymentGameServerViewModel>()
                 .ForMember(x => x.Amount, opt => opt.MapFrom(s => s.CashAmount));
-
+            Mapper.CreateMap<News, NewsViewModel>()
+                            .ForMember(x => x.UserName, opt => opt.MapFrom(source => source.User.UserName));
             this.MapPagedList<HelpDeskRequest, HelpDeskRequestViewModel>();
             this.MapPagedList<HelpDeskRequestComment, HelpDeskRequestCommentViewModel>();
             this.MapPagedList<ApplicationUser, SimpleApplicationUserViewModel>();
