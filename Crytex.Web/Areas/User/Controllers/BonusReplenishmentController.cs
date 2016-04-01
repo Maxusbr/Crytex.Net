940858abe0b9d79df79b9b8fdb40e9a4e4cd94c1
Maxusbr <max_73@inbox.ru>
2016-04-01 16:56:43 +0300

    add LongTermDiscountController&BonusReplenishmentController

---
+++ Crytex.Web/Areas/User/Controllers/BonusReplenishmentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AutoMapper;
+using Crytex.Service.IService;
+using Crytex.Web.Models.JsonModels;
+
+namespace Crytex.Web.Areas.User.Controllers
+{
+    public class BonusReplenishmentController : UserCrytexController
+    {
+        private readonly IDiscountService _discountService;
+
+        public BonusReplenishmentController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(IEnumerable<BonusReplenishmentViewModel>))]
+        public IHttpActionResult Get()
+        {
+            var allBonusReplenishments = _discountService.GetAllBonusReplenishments();
+            var models = Mapper.Map<IEnumerable<BonusReplenishmentViewModel>>(allBonusReplenishments);
+
+            return Ok(allBonusReplenishments);
+        }
+    }
+}
